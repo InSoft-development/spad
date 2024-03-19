92c3e541bb0f1c37c4882d2f0bdb2f3e68d39b26
matvey <ososkov@inctrl.ru>
2024-03-19 16:11:42 +0300

    correct logic of spad_cp - copy dirs recursively too, fix some bugs

--- spad_cp.py
+++ spad_cp.py
@@ -14,36 +14,35 @@ if __name__ == '__main__':
     args = parse_args()
     f1 = args.f1
     f2 = args.f2
-    if re.match(r'^task:.*',f1) : #local file in nearby task directory
+    filename=""
+
+    if re.match(r'^task:.*',f1) : #file in task directory in same workflow
+        #format <task>:<path>
         filename = f1.split(":")[1]
         cwd = os.getcwd()
-        if not re.match(r'\/opt\/spa\/data\/\w*\/\w*\/\w*', cwd):
+        if not re.match(r'\/opt\/spa\/data\/\w*\/\w*\/\w*', cwd): #we assue, that we are running from task of same workflow - othervise we can't locate needed workflow
             print("not in task dir: " + cwd)
             exit(1)
         f1 = cwd + "/../" + filename
-        if not os.path.isfile(f1):
-            print("no file " + f1)
-            exit(1)
-    else:
-        if not os.path.isfile(f1):
-            print("no file " + f1)
-            exit(1)
-
-    if re.match(r'^task:.*',f2) : #local file in nearby task directory
+    
+    if not os.path.isfile(f1) and not os.path.isdir(f1):
+        print("no file " + f1)
+        exit(1)
+    
+    if re.match(r'^task:.*',f2) : #file in task directory in same workflow
         filename = f2.split(":")[1]
         cwd = os.getcwd()
         if not re.match(r'\/opt\/spa\/data\/\w*\/\w*\/\w*',cwd):
             print("not in task dir: " + cwd)
             exit(1)
         f2 = cwd + "/../" + filename
-        try:
-            shutil.copyfile(f1, f2)
-            print("cp ", f1, " ", f2)
-        except OSError as err:
-            print("can not move file: " + str(err))
-    else:
-        try:
-            shutil.copyfile(f1, f2)
-            print("cp ", f1, " ", f2)
-        except OSError as err:
-            print("can not move file: " + str(err))
+
+    try:
+        print("cp ", f1, " ", f2)
+        if os.path.isdir(f1):
+            shutil.copytree(f1, f2, dirs_exist_ok=True)
+        elif os.path.isfile(f1):
+            shutil.copy(f1,f2)
+    except OSError as err:
+        print("can not copy file: " + str(err))
+    
