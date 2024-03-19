1e14be86813efe30c3aa2367ddd8e3b64a0d72fb
matvey <ososkov@inctrl.ru>
2024-03-19 16:15:19 +0300

    correct table in DB - remove timezone, correct names from tables for alarms, add missed timestamp into alarms - grafana fixed

--- alarms.py
+++ alarms.py
@@ -19,7 +19,7 @@ signal.signal(signal.SIGINT, handler_stop)
 t = datetime.now()
 print("data read")
 
-client = clickhouse_connect.get_client(host='10.23.0.87', username='default', password='asdf')
+client = clickhouse_connect.get_client(host='localhost', username='default', password='asdf')
 kks_df = client.query_df("SELECT * from kks")
 groups = set(kks_df["group"].astype(int)) - set([0])
 print(groups)
@@ -27,7 +27,7 @@ print(groups)
 header_p = ""
 for g in groups:
     header_p += "anomaly_time"+str(g)+" Nullable(Float64)," \
-        "anomaly_date"+str(g)+" Nullable(DateTime64(3,\'Europe/Moscow\'))," \
+        "anomaly_date"+str(g)+" Nullable(DateTime64)," \
         "P"+str(g)+" Float64, "
 header_p += "timestamp DateTime64(3,\'Europe/Moscow\')"
 client.command("DROP TABLE IF EXISTS sum_p")
@@ -46,13 +46,13 @@ while not terminate_flag:
     sum_p = pd.DataFrame()
     alarms = pd.DataFrame()
     for g in groups:
-        lstm = client.query_df("SELECT prob,count, timestamp FROM lstm_group"+str(g)+" order by timestamp desc limit 1")
+        lstm = client.query_df("SELECT target_value,count, timestamp FROM lstm_group"+str(g)+" order by timestamp desc limit 1")
         print("LSTM:\n", lstm)
 
         pot = client.query_df("SELECT probability,anomaly_time, timestamp FROM potential_predict_"+str(g)+" order by timestamp desc limit 1")
         print("Potential:\n", pot)
 
-        P = max(lstm["prob"][0], pot["probability"][0])
+        P = max(lstm["target_value"][0], pot["probability"][0])
         if pot["anomaly_time"][0] == "NaN":
             anomaly_time = lstm["count"][0]
         else:
@@ -62,6 +62,7 @@ while not terminate_flag:
         sum_p["anomaly_time"+str(g)] = [anomaly_time]
         sum_p["anomaly_date"+str(g)] = [anomaly_date]
         sum_p["P"+str(g)] = P
+        sum_p["timestamp"] = t
 
         if P > 95:
             data = "Превышение уровня аномальности, gr=" + str(g)
